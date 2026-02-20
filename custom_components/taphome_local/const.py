4cb7b7584a1462d3405019e6cae50f76898701d5
Adrian Mucska <adrianmucska@gmail.com>
2026-02-20 18:21:25 +0100

    Update const.py

--- custom_components/taphome_local/const.py
+++ custom_components/taphome_local/const.py
@@ -1,10 +1,17 @@
-"""Constants for the TapHome Local integration. / Konštanty pre integráciu TapHome Local."""
+"""Constants for the TapHome Local integration."""
 
 DOMAIN = "taphome_local"
+
 CONF_URL = "url"
 CONF_TOKEN = "token"
 CONF_DEBUG_LOGGING = "debug_logging"
-WEBHOOK_ID_PREFIX = "taphome_local_push_"
 
-# NOVÁ KONŠTANTA PRE SIGNÁL
+# --- NOVÉ KONŠTANTY PRE VÝBER ZARIADENÍ ---
+CONF_EXPOSE_AS_LIGHT = "expose_as_light"
+CONF_EXPOSE_AS_VALVE = "expose_as_valve"
+CONF_EXPOSE_AS_SWITCH = "expose_as_switch"
+CONF_EXPOSE_AS_COVER = "expose_as_cover" 
+# ------------------------------------------
+
+WEBHOOK_ID_PREFIX = "taphome_local_push_"
 SIGNAL_BUTTON_PRESSED = "taphome_button_pressed_{}"
